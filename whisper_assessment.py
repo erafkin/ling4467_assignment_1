a441dcff6a5fb15dc28aff31fdd96852e49e8c6f
Emma Rafkin <Emma.Rafkin@jhuapl.edu>
2025-10-14 13:47:38 -0400

    first pass works

---
+++ whisper_assessment.py
@@ -0,0 +1,58 @@
+from transformers import pipeline
+from faster_whisper import WhisperModel
+import torch
+from datasets import load_dataset, Audio
+import pandas as pd
+import time
+from jiwer import wer
+from tqdm import tqdm
+from whisper_normalizer.basic import BasicTextNormalizer
+normalizer = BasicTextNormalizer()
+
+def load_whisper_pipeline(model_str:str, faster:bool=False):
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
+    if faster:
+        return WhisperModel(model_str, device=device, compute_type="float16")
+    else:
+        return pipeline(
+            "automatic-speech-recognition",
+            model=model_str,
+            device=device
+        )
+
+
+
+def run_model(transcriber, dataset, output_name):
+    # Process audio files     
+    # Calculate WER and RTF     
+    # Return metrics and transcriptions      
+    df_rows = []
+    for row in tqdm(dataset):
+        start_time = time.time()
+        transcription = transcriber(row["audio"]["array"])
+        end_time = time.time()
+        rtf = (end_time - start_time) / (len(row["audio"]["array"])/row["audio"]["sampling_rate"])
+        transcription = normalizer(transcription["text"])
+        reference = normalizer(row["text"])
+        row_wer = wer(reference=reference, hypothesis=transcription)
+        df_rows.append([transcription, reference, row_wer, rtf])
+    df = pd.DataFrame(df_rows, columns=["transcription", "reference", "wer", "rtf"])
+    df.to_csv(output_name, index=False)
+
+
+def compare_models(models, dataset_options):
+    for dataset in dataset_options:
+        if dataset == "librispeech":
+            ds = load_dataset("openslr/librispeech_asr", split='test.clean', streaming=True).take(10)
+            test_ds = ds.cast_column("audio", Audio(sampling_rate=16_000))
+            test_ds = list(ds)
+
+        for model in models:
+            whisper = load_whisper_pipeline(model_str=model, faster=False)
+            run_model(whisper, test_ds, f"{dataset}_{model.split('/')[-1]}.csv")
+
+
+if __name__ == "__main__":
+    models = ["openai/whisper-tiny"]
+    ds_options = ["librispeech"]
+    compare_models(models, ds_options)
